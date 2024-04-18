513c99dff9100ae4b3eb6283fb560cc62bfd3c88
Inez Carrillo <inezcarrillo@Inezs-MacBook-Pro.local>
2024-04-18 18:48:02 -0500

    created the putDb and getDb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,38 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
+
+  const jatdb = await openDB ('jate', 1);
+
+  const tx = jateDB.transaction('jate', 'readwrite');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.putDb({ todo: content });
+
+  const result = await request;
+
+  console.log(result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error('getDb not implemented')
+  const jatdb = await openDB ('jate', 1);
+
+  const tx = jateDB.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.getDb({ todo: content });
+
+  const result = await request;
+
+  console.log(result);
+
+  return result;
+};
 
 initdb();
