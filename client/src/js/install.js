0a3336a13bed6fcfcac2278fbb5beb5fa80cf950
Inez Carrillo <inezcarrillo@Inezs-MacBook-Pro.local>
2024-04-18 18:47:42 -0500

    created the eventlisteners

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,22 @@
 const butInstall = document.getElementById('buttonInstall');
+const header = document.getElementById('header');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    butInstall.style.visibility = 'visible;'
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    event.prompt();
+    butInstall.setAttribute('disabled', true);
+    butInstall.textContent = 'Installed';
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    header.textContent = 'Successfully installed';
+    console.log('appinstalled', event);
+});
